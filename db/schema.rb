35f53afeaa02ea276bcfe591387eac6fe2195bea
Quang Dang <88885581+Gnauqoa@users.noreply.github.com>
2024-08-19 06:30:34 +0700

    Add default quantity for cart item.

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_18_232248) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_18_232526) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "cart_items", force: :cascade do |t|
-    t.integer "quantity"
+    t.integer "quantity", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "product_id"
