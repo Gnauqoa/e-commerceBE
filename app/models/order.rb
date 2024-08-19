f7df294903a0247827ad71853e94786b5cdf2a41
Quang Dang <88885581+Gnauqoa@users.noreply.github.com>
2024-08-19 10:41:19 +0700

    Update order and user discount model.

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,7 @@
 class Order < ApplicationRecord
   belongs_to :user
+  belongs_to :user_discount, optional: true
+  belongs_to :discount, optional: true
   has_many :order_items
 
   enum status: {
--- app/models/user_discount.rb
+++ app/models/user_discount.rb
@@ -1,4 +1,10 @@
 class UserDiscount < ApplicationRecord
   belongs_to :user
   belongs_to :discount
+  belongs_to :order, optional: true
+
+  enum status: {
+    avaliable: 1,
+    used: 0
+  }
 end
